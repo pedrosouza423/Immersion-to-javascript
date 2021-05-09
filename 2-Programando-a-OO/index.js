83d7bfb5e9ed1acae010e010bf6de49800c3fea9
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2021-05-09 07:38:34 -0300

    Foi criado uma classe e variáveis que instanciaram a classe

---
+++ 2-Programando-a-OO/index.js
@@ -0,0 +1,27 @@
+//Criar um serviço para o Byte Bank, o sistema deve ter um cliente com nome, saldo e cpf e realizar contas de uma conta corrente
+
+class Cliente{
+    nome
+    cpf
+    agencia
+    saldo
+    
+}
+
+const cliente1 = new Cliente()
+
+cliente1.nome = "Pedro"
+cliente1.cpf = 12345678901
+cliente1.agencia = 1001
+cliente1.saldo = 0
+
+const cliente2 = new Cliente()
+
+cliente2.nome = "Alice"
+cliente2.cpf = 22245678901
+cliente2.agencia = 1001
+cliente2.saldo = 0
+
+
+console.log(cliente1)
+console.log(cliente2)

812b3a29601f8b913f044c4efa61ad2a206f4fac
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2021-05-31 19:19:19 -0300

    Foi aplicado métodos para verificar se o sistema de autenticação existe como chave e se a chave é um método

--- 2-Programando-a-OO/SistemaAutenticacao.js
+++ 2-Programando-a-OO/SistemaAutenticacao.js
@@ -1,7 +1,15 @@
-//Ser autenticavel significa ter o método autenticar
+//Ser autenticavel significa ter o método autenticar - Ducky type
 
 export class SistemaAutenticacao {
     static login(autenticavel, senha) {
-        return autenticavel.autenticar(senha);
+        if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha);
+        }
+
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
